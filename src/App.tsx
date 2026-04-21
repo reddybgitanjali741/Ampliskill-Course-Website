50c939b968646b11218056a12bc0dfae79f917ba
reddybgitanjali741 <reddybgitanjali741@gmail.com>
2026-04-21 10:50:59 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./layout/Navbar/Navbar";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* Navbar yahan show hoga */}
+      <Navbar />
+
+      {/* Baaki content */}
+      <h1>My Website</h1>
+    </>
+  )
+}
+
+export default App
